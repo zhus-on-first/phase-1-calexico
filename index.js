c9c627905802dfdfdda0183f4e122434147fba68
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-08-10 08:53:51 -0700

    challenge 1 done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here...
+const menuItems = document.querySelector("#menu-items");
+
+//Challenge 1 (make/render 1 item, make list of items, get items)
+const renderItem = (item) => {
+  const span = document.createElement("span");
+  span.textContent = item.name;
+  menuItems.append(span);
+};
+
+const makeList = (items) => {
+  items.forEach((item) => {
+    renderItem(item);
+  });
+};
+fetch("http://localhost:3000/menu")
+  .then((response) => response.json())
+  .then((items) => makeList(items));
+
+// Challenge 2 render menu details
+
+// Challenge 3 add event listener to menu items
